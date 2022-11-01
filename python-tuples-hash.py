2de15a5781ab150abc9b319ad5f974dccac09895
Prasad Sanjay Patole <59770377+Psp29@users.noreply.github.com>
2022-11-01 09:30:48 +0530

    nothing new

--- python-tuples-hash.py
+++ python-tuples-hash.py
@@ -1,6 +1,7 @@
-# using pypy3(instead of python) gives right anser according to the community
+# using pypy3(instead of python) gives right answer according to the community
 
 n = int(input())
 numbers = map(int, input().split())
 tup1 = tuple(numbers)
 print(hash(tup1))
+print(numbers)
